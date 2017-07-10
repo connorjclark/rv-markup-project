53f4c690e95cb8c7c66dd4c20aa3df8612bd85bc
Connor Clark <cjamcl@gmail.com>
2017-07-09 20:29:43 -0700

    basic object -> integer scorer function: tagCountsToScore

--- src/__tests__/scorer.js
+++ src/__tests__/scorer.js
@@ -1,11 +1,27 @@
 /* eslint-env jest */
 
-import scorer from '../scorer'
+import {default as scorer, tagCountsToScore} from '../scorer'
 
 it('canary', () => {
   expect(1).toEqual(1)
 })
 
-it('basic', () => {
-  expect(scorer('')).toEqual(0)
+describe('scorer', () => {
+  it('basic', () => {
+    expect(scorer('')).toEqual(0)
+  })
+})
+
+describe('tagCountsToScore', () => {
+  const tester = (testName, tagCounts, expected) => {
+    it(testName, () => {
+      expect(tagCountsToScore(tagCounts)).toEqual(expected)
+    })
+  }
+  
+  tester('basic', {}, 0)
+  tester('1 div', {div: 1}, 3) // 1x3 = 3 points
+  tester('1 font', {div: 1}, 3) // 1x-1 = -1 points
+  tester('1 div, 2 p', {div: 1, p: 2}, 5) // 1x3 + 2x1 = 5 points
+  tester('1 div, 2 p', {div: 1, p: 2}, 5) // 1x3 + 2x1 = 5 points
 })
--- src/scorer.js
+++ src/scorer.js
@@ -1,3 +1,32 @@
+const tagValues = {
+  div: 3,
+  p: 1,
+  h1: 3,
+  h2: 2,
+  html: 5,
+  body: 5,
+  header: 10,
+  footer: 10,
+  font: -1,
+  center: -2,
+  big: -2,
+  strike: -1,
+  tt: -2,
+  frameset: -5,
+  frame: -5
+}
+
+export function tagCountsToScore (tagCounts) {
+  let score = 0
+
+  for (const tagName in tagCounts) {
+    const count = tagCounts[tagName]
+    score += count * tagValues[tagName]
+  }
+
+  return score
+}
+
 export default function scorer (htmlString) {
   return 0
 }
